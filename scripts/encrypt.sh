93f6509e0fcb62961b0a6cf31a388567d5884f4d
brianddk <brianddk@users.noreply.github.com>
2019-09-12 15:33:19 -0500

    Updated encrypt.sh for new files

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -3,11 +3,12 @@ cleartext=$1
 path=$(dirname $cleartext) || exit
 cyphertext=$(egrep "Comment: target " $1 | dos2unix | awk '{print $3}')
 source=$(realpath --relative-to="${path}/$(dirname cyphertext)" "$cleartext")
+cyphertext="${path}/${cyphertext}"
 script="s#^-----BEGIN PGP MESSAGE-----#-----BEGIN PGP MESSAGE-----\nComment: source $source#"
-if diff -d > /dev/null <( gpg2 -q -o- -d "${path}/${cyphertext}" ) "$cleartext"
+if [[ -f "${cyphertext}" ]] && diff -d > /dev/null <( gpg2 -q -o- -d "${cyphertext}" ) "$cleartext"
 then
   echo "Nothing to encrypt, $cleartext is unchanged"
 else
-  echo "Encrypting $cleartext to ${path}/${cyphertext}"
-  gpg2 -o- -a -r brianddk -e "$cleartext" | dos2unix | sed -e "$script" > "${path}/${cyphertext}"
+  echo "Encrypting $cleartext to ${cyphertext}"
+  gpg2 -o- -a -r brianddk -e "$cleartext" | dos2unix | sed -e "$script" > "${cyphertext}"
 fi
